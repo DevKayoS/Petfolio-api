c9a77f509cb3134cc8ff6b589d0cf4a35651cfa9
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-09-12 14:20:20 -0300

    feat: adicionado endpoint para deletar um pet por ID

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.Delete;
 using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.GetById;
 using Petfolio.Application.UseCases.Pets.Register;
@@ -64,6 +65,18 @@ namespace Petfolio.API.Controllers
             var response = useCase.Execute(id);
 
             return Ok(response);                
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var useCase = new DeletePetByIdUseCase();
+            useCase.Execute(id);
+            
+            return NoContent();
         }
     }
 }
---
+++ Petfolio.Application/UseCases/Pets/Delete/DeletePetByIdUseCase.cs
@@ -0,0 +1,6 @@
+namespace Petfolio.Application.UseCases.Pets.Delete;
+
+public class DeletePetByIdUseCase
+{
+    public void Execute(int id){}
+}
